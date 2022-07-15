da6e6eb72988ba0a3af5b74b54c143360933c349
Martin Wang <alpa.wong@gmail.com>
2022-07-16 01:31:34 +0800

    1. added logging module and settings

--- src/main/java/com/ebanking/payment/PaymentApplication.java
+++ src/main/java/com/ebanking/payment/PaymentApplication.java
@@ -1,14 +1,24 @@
 package com.ebanking.payment;
 
+import com.ebanking.payment.util.LoggingUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 @SpringBootApplication
 @EnableSwagger2
 public class PaymentApplication {
+	private  static final Logger LOGGER = Logger.getLogger(PaymentApplication.class.getName());
 
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws IOException {
+		LoggingUtil.initLogManager();
+		LOGGER.log(Level.INFO, "in main class");
+
 		SpringApplication.run(PaymentApplication.class, args);
 	}
 
