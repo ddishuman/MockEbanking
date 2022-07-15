8568c75781797268ea137e74a6e96844d57c7410
Martin Wang <alpa.wong@gmail.com>
2022-07-15 23:50:59 +0800

    1. added swagger dependencies and ignored security about swagger pages(but swagger-ui.html does not work, so wierd

--- src/main/java/com/ebanking/payment/PaymentApplication.java
+++ src/main/java/com/ebanking/payment/PaymentApplication.java
@@ -2,8 +2,10 @@ package com.ebanking.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PaymentApplication {
 
 	public static void main(String[] args) {
