69d1c393c521e4f24f4bc24826b8921e838280b7
Martin Wang <alpa.wong@gmail.com>
2022-07-15 10:18:03 +0800

    1. test github push

--- src/main/java/com/ebanking/payment/controller/HelloResource.java
+++ src/main/java/com/ebanking/payment/controller/HelloResource.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
-
+    // test
     @Autowired
     private AuthenticationManager authenticationManager;
 
